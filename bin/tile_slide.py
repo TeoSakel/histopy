2777024b973a1556e3f2b19e19ec231e0adfa28e
Teo Sakel <teo.sakel@gmail.com>
2019-04-03 19:26:43 -0400

    Extend functionality of tile_slide.py
    Add two new features:
      1. Filter which tiles to save based on tissue coverage
      2. Normalize tile coloring

--- bin/tile_slide.py
+++ bin/tile_slide.py
@@ -19,13 +19,29 @@ Options:
     -e PIXEL --overlap=PIXEL        Number of extra pixels to add to each interior edge of a tile [default: 1]
     -L --limit-bounds               Renders only non-empty slide region
     -m LEVEL --magnification=LEVEL  Magnification level to use for tiling [default: 20.]
+    -n --normalize                  Normalize staining (based on Macenko et al 2009)
+    --heuristic=VALUE               Heuristics to estimate tissue coverage separated by comma.
+                                    Possible values are: edge, dens.
+    -b VALUE --beta=VALUE           Tissue coverage lower-cutoff separated by comma. Must be in [0,1)
 """
 import os
 import csv
 import json
+# from functools import partial  # to change parameters defaults
 
+import numpy as np
+from PIL import Image
 from docopt import docopt
 from histopy.slide_parsers import OpenSlide, DeepZoomTiler
+from histopy.preprocessing import tissue_cover_edge, \
+        tissue_cover_dens, tissue_cover_gray, normalize_staining
+
+
+TILE_STATS = {
+    'cover_edge': tissue_cover_edge,
+    'cover_dens': tissue_cover_dens,
+    'cover_gray': tissue_cover_gray
+}
 
 
 def _check_option(option, argv, type_str, type_fun):
@@ -43,13 +59,20 @@ def _check_option(option, argv, type_str, type_fun):
     return ans
 
 
-def main(dz, outdir, tile_fmt='jpeg', jpg_qual=75):
+def main(dz, outdir, criteria=tuple(), normalize=None, tile_fmt='jpeg', jpeg_qual=75):
     """Loop through and save tile images in outdir."""
-    # TODO: add tile filters and tile normalization
     meta_file = os.path.join(outdir, "tile_metadata.csv")
-    meta_fields = ('col', 'row', 'tile_width', 'tile_height',
-                   'slide_level', 'slide_x', 'slide_y',
-                   'slide_width', 'slide_height')
+    meta_fields = ['col', 'row',                         # tile coordinates in grid
+                   'tile_width', 'tile_height',          # tile dimensions
+                   'slide_level', 'slide_x', 'slide_y',  # tile coordinates in slide
+                   'slide_width', 'slide_height']        # tile dimensions in slide
+    meta_fields.extend(TILE_STATS.keys())
+
+    if callable(normalize):
+        norm = normalize
+    else:
+        norm = np.asarray  # does nothing
+
     with open(meta_file, 'w+', newline='') as f:
         # TODO: write json instead?
         meta = csv.writer(f)
@@ -58,53 +81,96 @@ def main(dz, outdir, tile_fmt='jpeg', jpg_qual=75):
             col, row = address
             filename = "{x}_{y}.{ext}".format(x=col, y=row, ext=tile_fmt)
             filename = os.path.join(outdir, filename)
+            # get tile meta
             px, py = tile.size  # size of tile
             (x, y), lvl, (w, h) = dz.get_tile_coordinates(address)
             tile_stats = {'col': col, 'row': row,
                           'tile_width': px, 'tile_height': py,
                           'slide_level': lvl, 'slide_x': x, 'slide_y': y,
                           'slide_width': w, 'slide_height': h}
-            tile.save(filename, quality=jpg_qual)
+            tile = np.asarray(tile)
+            tile_stats.update({stat: estimator(tile)
+                               for stat, estimator in TILE_STATS.items()})
+            if any(tile_stats[crit] <= beta for crit, beta in criteria):
+                continue
+            tile = norm(tile)
+            tile = Image.fromarray(tile)
+            tile.save(filename, quality=jpeg_qual)
             meta.writerow(tile_stats[field] for field in meta_fields)
 
 
 if __name__ == '__main__':
     argv = docopt(__doc__, version='0.1')
-    # TODO: add (-f --force) option to skip overwritting files
-    # TODO: add (-v --verbose) option to give some feedback
+    # TODO: add `-f --force` option to skip overwritting files
+    # TODO: add `-v --verbose` option to give some feedback
+    # TODO: add more normalization methods (see preprocessing.py)
 
-    # Type-check options
+    outdir = argv['<outdir>']
+
+    # Type-check tile options
     tile_fmt = argv['--format']
     assert tile_fmt in ('jpeg', 'jpg', 'png'), \
         'Image format must be jpeg or png. "{}" given'.format(tile_fmt)
 
-    jpg_qual   = _check_option('quality',       argv, 'integer', int)
-    tile_size  = _check_option('size',          argv, 'integer', int)
-    tile_over  = _check_option('overlap',       argv, 'integer', int)
-    tile_zoom  = _check_option('magnification', argv, 'float',   float)
+    jpeg_qual = _check_option('quality',       argv, 'integer', int)
+    tile_size = _check_option('size',          argv, 'integer', int)
+    tile_over = _check_option('overlap',       argv, 'integer', int)
+    tile_zoom = _check_option('magnification', argv, 'float',   float)
     tile_limit = argv['--limit-bounds']
 
-    slide = OpenSlide(argv['<slide>'])
-    outdir = argv['<outdir>']
+    # Parse Normalization function
+    if argv['--normalize']:
+        normalize_fun = normalize_staining
+    else:
+        normalize_fun = None
 
-    dz = DeepZoomTiler(slide,
-                       tile_size=tile_size,
-                       overlap=tile_over,
-                       limit_bounds=tile_limit,
-                       magnification=tile_zoom)
+    # Setup inclusion criteria
+    # Parse Heuristic
+    if argv['--heuristic'] is None:
+        heuristics = ()
+    else:
+        heuristics = tuple('cover_' + h for h in argv['--heuristic'].split(','))
+        for c in heuristics:
+            if c not in TILE_STATS:
+                raise ValueError('Uknown heuristic `{}` specified'.format(c))
+
+    # Parse beta
+    if argv['--beta'] is None:
+        beta = ()
+    else:
+        beta = argv['--beta'].split(',')
+        try:
+            beta = list(map(float, beta))
+        except ValueError as e:
+            ValueError("Error while parsing beta parameter: " + e.args[0])
+        if len(beta) == 1:
+            beta *= len(heuristics)
+        assert len(beta) == len(heuristics), \
+            "Number of betas does not much number of heuristics"
+        assert all(0 <= b < 1 for b in beta), \
+            "Some betas are not in the [0, 1) interval"
+
+    criteria = tuple(zip(heuristics, beta))  # combine
+
+    # Read Silde and Tile
+    slide = OpenSlide(argv['<slide>'])
+
+    dz = DeepZoomTiler(slide, tile_size=tile_size, overlap=tile_over,
+                       limit_bounds=tile_limit, magnification=tile_zoom)
 
     # write dz parameters to tiling_metadata.json
     os.makedirs(outdir, exist_ok=True)
-    dz_meta = {'size': dz.tile_size,
-               'overlap': dz.overlap,
-               'bounds': dz.limit_bounds,
-               'grid': dz.shape,
-               'dimenions': dz.dimensions,
+    dz_meta = {'size':          dz.tile_size,
+               'overlap':       dz.overlap,
+               'bounds':        dz.limit_bounds,
+               'grid':          dz.shape,
+               'dimenions':     dz.dimensions,
                'magnification': dz.magnification,
-               'tile_format': tile_fmt,
-               'jpeg_qual': jpg_qual}
+               'normalize':     argv['--normalize'],
+               'tile_format':   tile_fmt,
+               'jpeg_qual':     jpeg_qual}
     dz_meta_path = os.path.join(outdir, 'tiling_params.json')
-    with open(dz_meta_path, 'w+') as fid:
+    with open(dz_meta_path, 'w') as fid:
         json.dump(dz_meta, fid)
 
-    main(dz, outdir, tile_fmt, jpg_qual)
+    main(dz, outdir, criteria=criteria, normalize=normalize_fun, tile_fmt=tile_fmt, jpeg_qual=jpeg_qual)
